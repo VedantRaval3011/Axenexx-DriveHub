9dcdc80d06a517d0118505e5765760008f6a79d2
VedantRaval3011 <vedantraval30@gmail.com>
2024-11-09 17:46:46 +0530

    Few assets added and base layout for sign-in and sign-up ready

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center">
+    <div className="flex-center">
       <div className="text-3xl text-brand">
         DriveHub - Only storage you need!
       </div>
